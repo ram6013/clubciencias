707ba763e48c66ca18e10cdff880804170f50184
ram6013 <ramongarciaprado2002@gmail.com>
2024-09-17 09:45:05 +0200

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import './index.css';
 import { motion } from 'framer-motion';
 
 function App() {
   return (
-    <motion.div className="h-screen w-full">
-      <h1>Club de ciencias</h1>
+    <motion.div className="bg-black h-screen w-full">
+      <h1 className='text-white'>Club de ciencias</h1>
     </motion.div>
   );
 }
